1f45152311bdfb32ff6f2b906a33e1ab5f71c1a3
youfa <vsyfar@gmail.com>
2025-02-19 21:58:01 +0800

    move video render to media/video
    Signed-off-by: youfa <vsyfar@gmail.com>

--- video/video_render.h
+++ video/video_render.h
@@ -1,6 +1,6 @@
 /*
  * video_render.h
- * Copyright (C) 2024 youfa <vsyfar@gmail.com>
+ * Copyright (C) 2022 youfa.song <vsyfar@gmail.com>
  *
  * Distributed under terms of the GPLv2 license.
  */
@@ -8,10 +8,24 @@
 #ifndef VIDEO_RENDER_H
 #define VIDEO_RENDER_H
 
+#include <memory>
+
+#include "media/foundation/media_sink_base.h"
+#include "media/foundation/message_object.h"
+
 namespace ave {
 namespace media {
 
-class VideoRender {};
+class VideoRender : public MediaFrameSink, public MessageObject {
+ public:
+  VideoRender() = default;
+  ~VideoRender() override = default;
+
+  // MediaSinkInterface implementation
+  void OnFrame(const std::shared_ptr<MediaFrame>& frame) override = 0;
+
+  virtual int64_t render_latency() { return 0; }
+};
 
 }  // namespace media
 }  // namespace ave
